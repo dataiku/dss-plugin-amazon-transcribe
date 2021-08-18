6a5a6b7f9ec83398745cb16b23fb003b54ebca78
Jérémy Plassmann <jeremy.plassmann@gmail.com>
2021-08-18 14:31:13 +0200

    debugging

--- python-lib/amazon_transcribe_api_client.py
+++ python-lib/amazon_transcribe_api_client.py
@@ -237,6 +237,7 @@ class AWSTranscribeAPIWrapper:
 
         while True:
             jobs = self.get_list_jobs(job_name_contains=recipe_job_id)
+            print('JOB_NAME_CONTAINS: ', recipe_job_id)
             print("JOBS: ", jobs)
             # loop over all jobs
             for job in jobs:
